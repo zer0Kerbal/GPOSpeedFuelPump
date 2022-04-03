712e601300497b42844794ad06f3044ea63d7901
Lisias T <me@lisias.net>
2022-04-02 21:35:00 -0300

    Automation Scripts

--- Source/GPOSpeedPump/GPOSpeedPump.cs
+++ Source/GPOSpeedPump/GPOSpeedPump.cs
@@ -1,20 +1,19 @@
 /*
-	This file is part of Goodspeed Automatic Fuel Pump /L Unleashed
+	This file is part of GPOSpeedPump /L Unleashed
 		© 2021 Lisias T : http://lisias.net <support@lisias.net>
 		© 2016-2019 hab136
 		© 2015 Geordiepigeonowner
 		© 2014 Gaius Goodspeed
 
-	Goodspeed Automatic Fuel Pump /L Unleashed is licensed as follows:
+	Historian /L Unleashed is licensed as follows:
 		* GPL 3.0 : https://www.gnu.org/licenses/gpl-3.0.txt
 
-	Goodspeed Automatic Fuel Pump /L Unleashed is distributed in the
-	hope that it will be useful, but WITHOUT ANY WARRANTY; without even
-	the implied warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	GPOSpeedPump /L Unleashed is distributed in the hope that it will be
+	useful, but WITHOUT ANY WARRANTY; without even the implied
+	warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 	You should have received a copy of the GNU General Public License 3.0 along
-	with Goodspeed Automatic Fuel Pump /L Unleashed.
-	If not, see <https://www.gnu.org/licenses/>.
+	with GPOSpeedPump /L Unleashed. If not, see <https://www.gnu.org/licenses/>.
 
 */
 
